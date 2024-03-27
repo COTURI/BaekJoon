c665d76376887fc5b3ae314b14df26cd11a31701
21-manic-006g <21-manic-006g@21-manic-006>
2024-03-27 17:47:08 +0900

    g

---
+++ BaekJoon/src/q18108/Main.java
@@ -0,0 +1,17 @@
+package q18108;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int year = sc.nextInt();
+		int y = year-543;
+		sc.close();
+		
+		
+		System.out.println(y);
+	}
+
+}
