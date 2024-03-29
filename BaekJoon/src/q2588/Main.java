139f0118422d679811f894994a461b8d8c3ac891
21-manic-006g <21-manic-006g@21-manic-006>
2024-03-29 17:44:30 +0900

    g

--- BaekJoon/src/q2588/Main.java
+++ BaekJoon/src/q2588/Main.java
@@ -6,18 +6,25 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int a = sc.nextInt();
-		int b = sc.nextInt();
+		String b = sc.next();
 		sc.close();
-		
-		
-		int c = (b % 10) * a;
-		int d = ((b % 100) / 10) * a;
-		int e = (b / 100) * a;
-		System.out.println(c);
-		System.out.println(d);
-		System.out.println(e);
-		System.out.println(a * b);
-		
+
+		int[] arr = new int[b.length()];
+
+		for (int i = 0; i < b.length(); i++) {
+			arr[i] = b.charAt(i) - '0';
+			// 아스키코드 문자열 '0'을 빼면 int 정수 값이 나옴
+
+		}
+		int result1 = a * arr[2];
+		int result2 = a * arr[1];
+		int result3 = a * arr[0];
+
+		System.out.println(result1);
+		System.out.println(result2);
+		System.out.println(result3);
+		System.out.println(a * Integer.parseInt(b));
+
 	}
 
 }
