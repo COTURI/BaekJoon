e507990e3e94b2497dbf1f8c5a48aa2c426b9e93
21-manic-006g <21-manic-006g@21-manic-006>
2024-04-19 17:42:54 +0900

    0419

---
+++ BaekJoon/src/q28278/Main.java
@@ -0,0 +1,32 @@
+package q28278;
+
+import java.util.Scanner;
+
+class mstack{
+	private int X;
+	
+	public void X(int X) {
+		this.X = X;
+	}
+}
+
+///푸는중
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		in
+		for(int i = 0; i<N; i++) {
+			String x = sc.nextLine();
+			switch(x){
+				case "1" :
+					
+					
+			}
+			
+		}
+	}
+
+}
