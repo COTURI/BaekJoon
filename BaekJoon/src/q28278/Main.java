f5177594fbef288011a7eb6d1c561307081e362a
21-manic-006g <21-manic-006g@21-manic-006>
2024-04-22 09:39:29 +0900

    0422

--- BaekJoon/src/q28278/Main.java
+++ BaekJoon/src/q28278/Main.java
@@ -17,7 +17,7 @@ public class Main {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
-		in
+	
 		for(int i = 0; i<N; i++) {
 			String x = sc.nextLine();
 			switch(x){
