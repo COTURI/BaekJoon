b0b82a62b7a6ab75399052b1e8cf3673b8f83c7b
21-manic-006g <21-manic-006g@21-manic-006>
2024-06-03 14:36:32 +0900

    0603

---
+++ BaekJoon/src/q10810/Main.java
@@ -0,0 +1,27 @@
+package q10810;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int N = sc.nextInt();
+		int[] arr = new int[N];
+
+		int M = sc.nextInt();
+		for (int i = 0; i < M; i++) {
+			int F = sc.nextInt();
+			int E = sc.nextInt();
+			int ball = sc.nextInt();
+
+			for (int j = F - 1; j < E; j++) {
+				arr[j] = ball;
+			}
+		}
+
+		for (int baguni : arr)
+			System.out.print(baguni + " ");
+	}
+}
